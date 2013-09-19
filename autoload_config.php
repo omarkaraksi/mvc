e50e743f57bf5fe431f363fab20f11a74c94d574
omarkaraksi <omar.karaksi09@gmail.com>
2013-09-19 10:11:50 +0200

    autoload_config.php

---
+++ autoload_config.php
@@ -0,0 +1,25 @@
+
+<?php 
+
+
+$AUTOLOAD_CLASS = array(
+					SERVER_ROOT.'/'.'forms',
+			 		SERVER_ROOT.'/'.'models',
+			        SERVER_ROOT.'/'.'models/entities', 
+			       	SERVER_ROOT.'/'.'lib/core',
+				   	SERVER_ROOT.'/'.'lib'
+				   	
+				    );
+$AUTOLOAD_CLASS_ = array( 
+			 		WEB_FOLDER.'/'.'controllers',
+			        WEB_FOLDER.'/'.'models',
+			        WEB_FOLDER.'/'.'models/entities', 
+			        WEB_FOLDER.'/'.'views',
+				    WEB_FOLDER.'/'.'lib/core',
+				   
+				    );				 			
+		
+
+$AUTOLOAD_CLASS_RECURSIVE= array( SERVER_ROOT.'/'.'lib/core/'  ) ;
+	
+?>
